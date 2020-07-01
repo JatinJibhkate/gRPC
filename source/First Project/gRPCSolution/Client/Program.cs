b10f1d52a3bdc290426c953ad34bb2bee66780f1
Jatin Jibhkate <j.jibhkate@tricentis.com>
2020-07-01 10:20:42 +0530

    First gRPC Client Server code

---
+++ source/First Project/gRPCSolution/Client/Program.cs
@@ -0,0 +1,33 @@
+﻿using Grpc.Core;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GrpcClient
+{
+    class Program
+    {
+        const string serverAddress = "127.0.0.1:50501";
+        
+        static void Main(string[] args)
+        {
+            //from Grpc.Core
+            Channel channel = new Channel(serverAddress, ChannelCredentials.Insecure);
+            channel.ConnectAsync().ContinueWith(task =>
+            {
+                if(task.Status == TaskStatus.RanToCompletion ||
+                   task.Status == TaskStatus.Canceled)
+                {
+                    Console.WriteLine("The client connected to the server successfully!");
+                }
+            });
+
+            var client = new Sample.SampleService.SampleServiceClient(channel);
+            channel.ShutdownAsync().Wait();
+            Console.ReadKey();
+        }
+    }
+}
