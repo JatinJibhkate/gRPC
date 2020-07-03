d2a857bcc79f47751e3eb6e5a0927ccf414903c6
Jatin Jibhkate <j.jibhkate@tricentis.com>
2020-07-03 23:23:36 +0530

    Projects

---
+++ source/Server Streaming/gRPCSolution/Client/Program.cs
@@ -0,0 +1,49 @@
+﻿using Grpc.Core;
+using Primenumberdecomposition;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GrpcClient
+{
+    class Program
+    {
+        const string serverAddress = "127.0.0.1:50501";
+        
+        static async Task Main(string[] args)
+        {
+            //from Grpc.Core
+            Channel channel = new Channel(serverAddress, ChannelCredentials.Insecure);
+            await channel.ConnectAsync().ContinueWith(task =>
+            {
+                if(task.Status == TaskStatus.RanToCompletion ||
+                   task.Status == TaskStatus.Canceled)
+                {
+                    Console.WriteLine("The client connected to the server successfully!");
+                }
+            });
+
+            int inputNumber = 200;
+
+            //create an instance of number service stub client
+            var client = new NumberService.NumberServiceClient(channel);
+
+            //call the prime number decomposition
+            var response = client.GetPrimeNumberDecomposition(new Request { NumberData = new NumberData { Number = inputNumber } });
+
+            StringBuilder builder = new StringBuilder();
+            while (await response.ResponseStream.MoveNext())
+            {
+                builder.Append(response.ResponseStream.Current.NumberData.Number.ToString() + "*");
+            }
+
+            Console.WriteLine($"Prime number decomposition for number { inputNumber } = { builder.ToString().TrimEnd('*') }");
+
+            channel.ShutdownAsync().Wait();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ source/Server Streaming/gRPCSolution/Server/NumberServiceEx.cs
@@ -0,0 +1,44 @@
+﻿using Grpc.Core;
+using Primenumberdecomposition;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Primenumberdecomposition.NumberService;
+
+namespace Grpc.Server
+{
+    public class NumberServiceEx : NumberServiceBase
+    {
+        public override async Task GetPrimeNumberDecomposition(Request request, IServerStreamWriter<Response> responseStream, ServerCallContext context)
+        {
+            //prime number decomposition algorithm
+            int factor = 2;
+            int number = request.NumberData.Number;
+            while (number > 1)
+            {
+                // if factor evenly divides into number
+                if (number % factor == 0)
+                {
+
+                    // this is a factor
+                    await responseStream.WriteAsync(new Response
+                    {
+                        NumberData = new NumberData
+                        {
+                            Number = factor
+                        }
+                    });
+
+                    // divide number by factor so that we have the rest of the number left.
+                    number = number / factor;
+                }
+                else
+                {
+                    factor = factor + 1;
+                }
+            }
+        }
+    }
+}
